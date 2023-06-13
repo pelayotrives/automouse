c5c45cb6148a58a99ec99f55f43425cfcf2c9e73
Pelayo Trives <pelayotrives@gmail.com>
2023-06-14 00:19:52 +0200

    fix: humanize coordinates generation.

--- automouse.py
+++ automouse.py
@@ -1,10 +1,15 @@
 import pyautogui
 import time
+import random
 
 while True:
     try:
         # Get the current mouse position.
         x, y = pyautogui.position()
+
+        # Generate random coordinates within a range
+        random_x = random.randint(x - 10, x + 10)
+        random_y = random.randint(y - 10, y + 10)
 
         # Move the mouse slightly and then return to the original position.
         pyautogui.moveTo(x + 10, y + 50)
