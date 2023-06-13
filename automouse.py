75ed28a8a850194388c71c45bd435e45cef934a0
Pelayo Trives <pelayotrives@gmail.com>
2023-06-14 00:15:41 +0200

    feat: script instructions modified and new coordinated included.

--- automouse.py
+++ automouse.py
@@ -3,14 +3,14 @@ import time
 
 while True:
     try:
-        # Get the current mouse position
+        # Get the current mouse position.
         x, y = pyautogui.position()
 
-        # Move the mouse slightly to the right and then return to the original position
+        # Move the mouse slightly and then return to the original position.
         pyautogui.moveTo(x + 10, y + 50)
         pyautogui.moveTo(x, y)
 
-        # Wait 5 seconds before repeating the movement
+        # Wait 5 seconds before repeating the movement.
         time.sleep(5)
     except KeyboardInterrupt:
         break
